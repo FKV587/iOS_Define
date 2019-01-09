6ccad3d106e1954ef55728ad2306c8629dd841a9
fukai <>
2019-01-09 11:34:52 +0800

    0.0.14

--- DaYi_Defines/DaYi_Defines/Category/UIViewController+UINavigationStyle.h
+++ DaYi_Defines/DaYi_Defines/Category/UIViewController+UINavigationStyle.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
+@class QMUINavigationControllerDelegate;
 @interface UIViewController (UINavigationStyle)<QMUINavigationControllerDelegate>
 
 @end
